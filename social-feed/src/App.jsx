e03b695a6dc59814e1ff72deeed1d020a8fd849b
Naiara <naiara@mentorspace.dev>
2022-09-27 15:26:23 -0300

    initial commit

---
+++ social-feed/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+
+import './styles.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+        <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
