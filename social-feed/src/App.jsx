486c03280ee54019483e9224bfc11dac36d89efc
Naiara <naiara@mentorspace.dev>
2022-09-29 15:02:58 -0300

    Criando componente de Post

--- social-feed/src/App.jsx
+++ social-feed/src/App.jsx
@@ -1,8 +1,10 @@
 import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar";
 
 import styles from './App.module.css';
 import './global.css';
-import { Sidebar } from "./components/Sidebar";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post />
 
         </main>
       </div>
