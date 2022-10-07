ca32bf06fdda339c3fd36e985c43bc5c56adb03d
Naiara <naiara@mentorspace.dev>
2022-10-07 13:19:38 -0300

    Utilizando props para comunicação de componentes

--- social-feed/src/components/Comment.jsx
+++ social-feed/src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/naiaraborges.png" alt="" />
@@ -16,7 +21,7 @@ export function Comment({ content }) {
                Publicado há 1h
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
--- social-feed/src/components/Post.jsx
+++ social-feed/src/components/Post.jsx
@@ -35,6 +35,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+   console.log(`Deletar ${comment}`);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -79,7 +83,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment}/>
+          return (
+          <Comment
+           key={comment}
+           content={comment}
+           onDeleteComment={deleteComment}
+          />
+         )
         })}
       </div>
     </article>
