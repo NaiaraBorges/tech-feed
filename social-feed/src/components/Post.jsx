01c6c5153eb8ced6bfdc4a57f423b84a43b44cb1
Naiara <naiara@mentorspace.dev>
2022-10-07 15:02:02 -0300

    Criando função para remover comentários

--- social-feed/src/components/Post.jsx
+++ social-feed/src/components/Post.jsx
@@ -35,8 +35,12 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(comment) {
-   console.log(`Deletar ${comment}`);
+  function deleteComment(commentToDelete) {
+   const commentsWithoutDeleteOne = comments.filter(comment => {
+    return comment != commentToDelete;
+   })
+
+   setComments(commentsWithoutDeleteOne);
   }
 
   return (
